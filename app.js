fb82bc844954c854e4e948f4f6522d319a0ae7e4
DavidE008 <114615559+DavidE008@users.noreply.github.com>
2026-03-25 21:43:21 -0500

    Add routes for employee and doctor

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ require('dotenv').config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.set('view engine', 'ejs');
 
 const patientRoutes = require('./routes/patient');
+const employeeRoutes = require('./routes/employee');
+const doctorRoutes = require('./routes/doctor');
 
 app.use('/patient', patientRoutes);
+app.use('/employee', employeeRoutes);
+app.use('/doctor', doctorRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
